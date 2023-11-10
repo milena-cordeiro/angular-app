49a850383f3c44b1b91c69eee4a851614c1f011a
Milena Cordeiro <mcs9823@gmail.com>
2023-11-10 15:55:32 -0300

    fazendo requisição e renderizando imagens

--- src/app/hello-world.component.ts
+++ src/app/hello-world.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'hello-world',
@@ -7,11 +7,22 @@ import { Component } from "@angular/core";
   templateUrl: './hello-world.component.html',
   imports: [CommonModule],
 })
-export class HelloWorldComponent {
+export class HelloWorldComponent implements OnInit {
   greeting: string = 'Hello T30';
   isVisible: boolean = true;
   programingLanguages: string[] = ['JavaScript', 'TypeScript', 'Java', 'C#', 'Python', 'PHP'];
   peoples = [{name: 'João', age: 18}, {name: 'Maria', age: 20}, {name: 'José', age: 30}]
+  dogsImages: string | null = null;
+  isLoading: boolean = false;
+
+  async ngOnInit(): Promise<void> {
+    this.isLoading = true;
+    const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    if(response.status !== 200) return;
+    const data = await response.json();
+    this.dogsImages = data.message;
+    this.isLoading = false;
+  } // seria o equivalente ao componentDidMount em React
 
   sayHello() {
     alert('dizendo olá')

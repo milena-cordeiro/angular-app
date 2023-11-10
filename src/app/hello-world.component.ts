c9b123732a24a564be10775e690fd235aa415ed7
Milena Cordeiro <mcs9823@gmail.com>
2023-11-10 15:14:25 -0300

    criando event handlers

--- src/app/hello-world.component.ts
+++ src/app/hello-world.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 
 @Component({
   selector: 'hello-world',
   standalone: true,
-  template: `
-    <h2>{{greeting}}</h2>
-  `
+  templateUrl: './hello-world.component.html',
+  imports: [CommonModule],
 })
 export class HelloWorldComponent {
   greeting: string = 'Hello T30';
+  isVisible: boolean = true;
+
+  sayHello() {
+    alert('dizendo olá')
+  }
+
+  sayGoodbye() {
+    alert('dizendo tchau')
+  }
+
+  toggleVisibility() {
+    this.isVisible = !this.isVisible;
+  }
 }
